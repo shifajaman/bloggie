00992087b7c07b6bf3f5c10f3100fcb120c13b44
shifa jaman <shifaba94@gmail.com>
2022-01-22 10:57:14 +0530

    Setup project

---
+++ src/main/java/bloggie/BloggieApplication.java
@@ -0,0 +1,13 @@
+package bloggie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggieApplication.class, args);
+	}
+
+}
