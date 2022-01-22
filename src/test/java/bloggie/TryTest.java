502d4a05d2151aa9077ae0988d6fd31aa30118c4
shifa jaman <shifaba94@gmail.com>
2022-01-22 11:15:23 +0530

    add sample test

---
+++ src/test/java/bloggie/TryTest.java
@@ -0,0 +1,13 @@
+package bloggie;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TryTest {
+    @DisplayName("sample test for testing CI/CD")
+    @Test
+    void learn(){
+        Assertions.assertEquals(1, 1);
+    }
+}
