b8f677add06baa5bdc4236b73549ce1c94fbb0da
shifa jaman <shifaba94@gmail.com>
2022-01-27 10:19:00 +0530

    handle failure for existing user error

--- src/main/java/bloggie/controllers/advice/GlobalControllerAdvice.java
+++ src/main/java/bloggie/controllers/advice/GlobalControllerAdvice.java
@@ -3,6 +3,7 @@ package bloggie.controllers.advice;
 import bloggie.contracts.response.UserCreatedResponse;
 import bloggie.errors.BloggieError;
 import bloggie.errors.ErrorCodes;
+import bloggie.errors.InvalidDataException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +19,22 @@ import java.util.stream.Collectors;
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<UserCreatedResponse> validation(MethodArgumentNotValidException exception){
+    public ResponseEntity<UserCreatedResponse> validation(MethodArgumentNotValidException exception) {
         var msg = exception.getAllErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .filter(Objects::nonNull)
-                .filter(x->!x.isBlank()).collect(Collectors.joining(", "));
-       var invalidUserErr = new BloggieError(msg, ErrorCodes.INVALID_USER_NAME.name());
-       var res =  new UserCreatedResponse(null, Collections.singletonList(invalidUserErr));
-       return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
-
+                .filter(x -> !x.isBlank()).collect(Collectors.joining(", "));
+        var invalidUserErr = new BloggieError(msg, ErrorCodes.INVALID_USER_NAME.name());
+        var res = new UserCreatedResponse(null, Collections.singletonList(invalidUserErr));
+        return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(value = InvalidDataException.class)
+    public ResponseEntity<UserCreatedResponse> validation(InvalidDataException exception) {
+        var msg = exception.getMessage();
+        var invalidUserErr = new BloggieError(msg, ErrorCodes.INVALID_USER_NAME.name());
+        var res = new UserCreatedResponse(null, Collections.singletonList(invalidUserErr));
+        return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+    }
+
 }
--- src/test/java/bloggie/controllers/UsersControllerTest.java
+++ src/test/java/bloggie/controllers/UsersControllerTest.java
@@ -2,6 +2,7 @@ package bloggie.controllers;
 
 import bloggie.controllers.advice.GlobalControllerAdvice;
 import bloggie.domain.User;
+import bloggie.errors.InvalidDataException;
 import bloggie.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -50,6 +51,25 @@ class UsersControllerTest {
 
         var responseBody = mockMvc.perform(createRequest(requestBody))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn().getResponse().getContentAsString();
+
+        assertEquals(expectedResp.trim(), responseBody.trim());
+    }
+    @Test
+    @DisplayName("create user should fail by returning 4xx status code for existing user name")
+    void createUserShouldFailForExistingUserName() throws Exception {
+        var requestBody = """
+                {"name":"shifa"}
+                """;
+        var expectedResp = """
+                {"user":null,"errors":[{"description":null,"errorCode":"INVALID_USER_NAME"}]}
+                """;
+        var inputUser = new User("shifa");
+
+        Mockito.when(service.createUser(inputUser)).thenThrow(InvalidDataException.class);
+
+        var responseBody = mockMvc.perform(createRequest(requestBody))
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
                 .andReturn().getResponse().getContentAsString();
 
         assertEquals(expectedResp.trim(), responseBody.trim());
